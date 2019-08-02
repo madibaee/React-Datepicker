744a9fb92d06b4cae93765884dd4fd11d67fb484
Mohammad Ali Dibaee <adibaee@gmail.com>
2019-08-02 19:49:00 +0430

    Compile scss file

--- dist/parseString.js
+++ dist/parseString.js
@@ -9,7 +9,9 @@ var _persianDate = _interopRequireDefault(require("persian-date"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { keys.push.apply(keys, Object.getOwnPropertySymbols(object)); } if (enumerableOnly) keys = keys.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(source, true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(source).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
@@ -28,7 +30,7 @@ var _default = function _default(input, persianDate, format) {
 
     for (var i = 0; i < format.length; i++) {
       var value = camelize(params[i + 1]);
-      output = _objectSpread({}, output, parse(toLatinNumber(value), format[i]));
+      output = _objectSpread({}, output, {}, parse(toLatinNumber(value), format[i]));
     }
 
     _persianDate["default"].calendarType = persianDate.calendarType;
